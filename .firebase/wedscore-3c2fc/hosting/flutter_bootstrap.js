66301438b7b86c8efc712f60324c5842b6844493
abhishekathena29 <abhishek.kumar@athenaeducation.co>
2026-04-15 15:43:12 +0530

    changes

--- .firebase/wedscore-3c2fc/hosting/flutter_bootstrap.js
+++ .firebase/wedscore-3c2fc/hosting/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3723517693"
+    serviceWorkerVersion: "952790847"
   }
 });
